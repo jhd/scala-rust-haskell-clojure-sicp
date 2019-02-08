570f51ac7a5861cfd9b8b6a25ea24e4af5295435
jhd <ianxp0@gmail.com>
2019-02-08 21:27:09 +0000

    Add Clojure solution for 2.68
    Huffman decode/encode

--- 2.68-9/clojure/sol.clj
+++ 2.68-9/clojure/sol.clj
@@ -18,6 +18,42 @@
     (symbol-leaf tree)
     (nth tree 2)))
 
+(defn left-branch [tree] (first tree))
+
+(defn right-branch [tree] (second tree))
+
+(defn choose-branch [bit branch]
+  (cond (= bit 0) (left-branch branch)
+        (= bit 1) (right-branch branch)
+        :else '() ))
+
+(defn encode-symbol [sym tree]
+  (cond (= tree '()) '()
+        (= (symbol-leaf tree) sym) '()
+        true (if (.contains (symbols (left-branch tree)) sym) 
+          (cons 0 (encode-symbol sym (left-branch tree)))
+          (cons 1 (encode-symbol sym (right-branch tree))))))
+
+
+(defn decode [bits tree]
+  (defn decode-1 [bits current-branch]
+    (println bits " : " current-branch)
+    (if (= bits '())
+      '()
+      (let [next-branch (choose-branch (first bits)
+                                       current-branch)]
+            (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (rest bits) tree))
+              (decode-1 (rest bits) next-branch)))))
+  (decode-1 bits tree))
+
+(defn encode [message tree]
+  (if (= message '())
+    '()
+    (concat (encode-symbol (first message) tree)
+            (encode (rest message) tree))))
+
 (defn make-code-tree [left right]
   (list left
         right
@@ -25,13 +61,11 @@
         (+ (weight left) (weight right))))
 
 (defn adjoin-set [x pset]
-  ;(println "as" x " : " pset)
   (cond (= pset `()) (list x)
         (< (weight x) (weight (first pset))) (cons x pset)
         :else (cons (first pset) (adjoin-set x (rest pset)))))
 
 (defn make-leaf-set [pairs]
-  ;(println pairs)
   (if (= pairs `())
     `()
     (let [pair (first pairs)]
@@ -39,7 +73,6 @@
                   (make-leaf-set (rest pairs))))))
 
 (defn successive-merge [pairs]
-  ;(println "SM" pairs)
   (cond (= pairs `()) `()
         (= (rest pairs) `()) (first pairs)
         :else (successive-merge
@@ -52,11 +85,8 @@
   (println freq-list)
   (successive-merge (make-leaf-set freq-list)))
 
-
-(defn encode [])
-
-(defn decode [])
-
 (defn -main []
   (let [tree (generate-huffman-tree (reverse `(("NA" 16) ("YIP" 9) ("SHA" 3) ("A" 2) ("GET" 2) ("JOB" 2) ("BOOM" 1) ("WAH" 1))))]
-  (println tree)))
+  (println tree)
+  (println (encode (list "NA" "YIP" "SHA") tree))
+  (println (decode (encode (list "NA" "YIP" "SHA") tree) tree))))
